bd0667964397c8b7caca45fd2ca3524362aa0cad
Raj malviya <rajm45928@gmail.com>
2026-09-11 17:59:52 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,39 @@
+print("Raj malviya.", "i am 21 year old.")
+print(23)
+
+print(25+23)
+print(25-21)
+print(25*21)
+print(23/21)
+print(22*21)
+print(1000*1)
+
+name = "Raj malviya"
+age = 21
+print(name , age)
+print("my name is :", name)
+print("my age is :", age)
+
+print(type(name))
+print(type(age))
+
+print('rj')
+print("rj")
+print("'rj'")
+
+age =21
+old = False
+a = None
+print(type(old))
+print(type(a))
+
+
+a = 2
+b = 3
+sum = a + b
+print("value of sum is :",sum)
+
+a = 5
+b = 1
+diff = a - b
+print("value of diff is :",diff)
