82119672c38c9e0e71bbf01b1e1a60616991367c
Raj malviya <rajm45928@gmail.com>
2026-09-13 15:29:09 +0530

    Add files via upload

---
+++ python5.py
@@ -0,0 +1,17 @@
+# str = "hello"
+# str1 = "world"
+# print(str+ str1)
+# print(len(str1))
+
+# str = "heyeeee"
+# ch = str[2]
+# print(ch)
+# print(str[1])
+# print(str[0:4])
+# print(str[0:2:1])
+ 
+# name = input("enter name")
+# print(len(name)) 
+
+str = " Hi, $ I am the $ symbol"
+print(str.count("$"))
