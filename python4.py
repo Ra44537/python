36b5e557f652f06de8bcfbd4d7a7b3e56c0ee132
Raj malviya <rajm45928@gmail.com>
2026-09-13 15:00:04 +0530

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,27 @@
+# name = input("enter your name: ")
+# print("hey ", name)
+
+# name = input("enter your name :")
+# age = int(input("enter your age :"))
+# clas = input("enter your class :")
+# print(name , clas , age)
+
+
+# sum of two numbers
+# a = int(input ("enter a value of a :"))
+# b = int(input ("enter a value of b :"))
+# sum = a + b
+# print("sum of a and b is ", sum)
+
+
+
+# square of a area
+# num = int(input("enter a number : "))
+# print("square of your area is", num * num)
+ 
+
+#greater then equal to question
+a = int(input ("enter a value of a :"))
+b = int(input ("enter a value of b: "))
+c = (a >= b)
+print(c)
