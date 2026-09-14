6d7bfdbd88c0187d80334a09d6ab81de2b3cd419
Raj malviya <rajm45928@gmail.com>
2026-09-14 15:31:55 +0530

    Add files via upload

---
+++ python6.py
@@ -0,0 +1,39 @@
+# age = int(input("Please tell your age: "))
+
+# if(age >= 18):
+#     if(age >= 80):
+#         print("cannot drive")
+#     else:
+#         print("can drive")
+# else:
+#     print("cannot drive")  
+
+# odd and even
+# num = int(input ("please enter a number:"))
+# rem = num % 2
+# if(rem == 0):
+#     print("even number")
+# else:
+#     print("odd number")    
+
+
+#greater of 3 numbers
+# a = int(input("enter value of a:"))
+# b = int(input("enter value of b:"))
+# c = int(input("enter value of c:"))
+
+# if(a>=b and a>=c ):
+#     print(" a is greater")
+# elif(b >= a):
+#     print("b is greater")
+# else:
+#     print("c is greater")    
+        
+
+#multiple of 7
+a = int(input("enter a number : "))
+
+if(a % 7 == 0):
+    print("multiple of 7")
+else:
+    print("not a multiple of 7")    
