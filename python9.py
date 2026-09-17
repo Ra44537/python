75aafb85e295d348ab1c91903c4bb71e46ab4d88
Raj malviya <rajm45928@gmail.com>
2026-09-17 18:36:51 +0530

    Add files via upload

---
+++ python9.py
@@ -0,0 +1,102 @@
+# count = 1
+# while count <= 5:
+#     print("hello")
+#     count += 1
+
+#     print(count)
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+
+#print 100 to 1
+
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+
+# table of 3
+
+# i = 1
+# a = int(input("enter"))
+# while i <= 10:
+#     print(i*a)
+#     i += 1
+
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# idx = 0
+
+# while idx < len(nums):
+#     print(nums[idx])
+#     idx += 1 
+
+
+
+# nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+
+
+# x = 36
+
+
+# i = 0
+# while i < len(nums):
+#     if  (nums[i] == x ):
+#         print("found at idx", i)
+#     i += 1
+
+# i = 1
+# while i<= 5:
+#     print(i)
+#     if (i == 3):
+#         break
+   
+#     i += 1
+
+# veggies = ["potato", "brijal", "cucumber", "brinjal"]
+
+# for val in veggies:
+#     if ( val == "brijal"):
+#         print("find")
+#         break
+#     print(val)
+# else:
+#     print("END OF LOOP")
+
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# for val in nums:
+#     print(val)
+
+# seq = range(5)
+# print(seq[1])
+
+# for i in range(100, 1, -1):
+#     print(i)
+
+# i = 1
+# n = int(input("enter no :"))
+# while (i<=10):
+#     print(i*n)
+#     i += 1
+
+# n= 5
+# sum =0 
+# for i in range(1, n+1):
+#     sum += i
+    
+# print("total sum =", sum)
+
+n = 5
+fact = 1
+for i in range(1 , n+1):
+    fact *= i 
+    
+
+    print("facct", fact)
+
