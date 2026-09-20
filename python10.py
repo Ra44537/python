f5b7d9c6fdb813fb8fb66c5a220ccd6bebd43226
Raj malviya <rajm45928@gmail.com>
2026-09-20 16:25:03 +0530

    Add files via upload

---
+++ python10.py
@@ -0,0 +1,78 @@
+# def sum_c(a,b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# sum_c(2,3)
+
+
+
+
+# def print_hello():
+#     print("hello")
+
+# print_hello()   
+
+
+
+
+# def sum_c(a,b,c):
+#     sum = a+b+c
+#     avg = sum/3
+#     print(avg)
+
+# sum_c(1,2,3)    
+
+
+
+
+
+# cities = ["bhopal", "indore"]
+# heros = ["raj", "rahul"]
+
+# def list_len(list):
+#     print(len(list))
+
+# list_len(cities)    
+# list_len(heros)
+
+
+
+
+# cities = ["bhopal", "indore"]
+# heros = ["raj", "rahul"]
+
+# print(heros[0], end="  ")
+# print(heros[1], end="  ")
+
+# def cal(a):
+#     fact = 1
+#     for i  in range (1, a + 1 ):
+#         fact *= i
+#         print(fact)
+
+
+
+
+
+# cal(5)
+
+# def converter(usd):
+#     inr_val = usd * 90
+#     print("usd", usd , " =  inr", inr_val)
+
+
+
+
+
+# converter(10000)    
+# def  a(int(input("enter no"))):
+#     if (a /2):
+#     print("even")
+# else:
+#     print("odd")   
+#     print(a)
+
+
+
+ 
